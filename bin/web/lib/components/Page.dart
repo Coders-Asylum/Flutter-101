327898fe4589161adc239cb7c414ccdc76046a87
MavericK099 <adithyashetty032@gmail.com>
2021-06-05 10:47:54 +0530

    initial commit

---
+++ bin/web/lib/components/Page.dart
@@ -0,0 +1,37 @@
+// import 'package:flutter/material.dart';
+//
+// /// Class to crea
+// class Page {
+//   late final double screenHeight;
+//   late final double screenWidth;
+//
+//   late final double pageHeight;
+//   late final double pageWidth;
+//   late final ScrollController scrollController;
+//
+//
+//   Widget page(BuildContext context) {
+//     return Scrollbar(
+//       controller: scrollController,
+//       child: SingleChildScrollView(
+//         controller: scrollController,
+//         child: Container(
+//           height: ,
+//         ),
+//       ),
+//     );
+//   }
+//
+//   @factory
+//   PageTile tile ()
+//
+// }
+//
+// abstract class PageTile {
+//   Widget tile (BuildContext context){
+//     return Container(
+//
+//     ),
+//   }
+//
+// }
