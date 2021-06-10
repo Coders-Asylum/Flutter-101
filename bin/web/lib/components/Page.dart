a5929198b482ae01b9a0555c2c54020c0a640e63
MavericK099 <adithyashetty032@gmail.com>
2021-06-10 21:56:17 +0530

    Update Page.dart

--- bin/web/lib/components/Page.dart
+++ bin/web/lib/components/Page.dart
@@ -1,37 +1,54 @@
 // import 'package:flutter/material.dart';
 //
-// /// Class to crea
-// class Page {
-//   late final double screenHeight;
-//   late final double screenWidth;
+// class PageTile extends StatefulWidget {
+//   final Size safeArea;
+//   final Color tileBackgroundColor;
+//   final List<Widget> tileBackground;
 //
-//   late final double pageHeight;
-//   late final double pageWidth;
-//   late final ScrollController scrollController;
+//   PageTile({
+//     this.safeArea = const Size(1440, 1240),
+//     this.tileBackgroundColor = Colors.transparent,
+//     this.tileBackground = const [],
+//   });
 //
+//   final Widget child = tileChild();
 //
-//   Widget page(BuildContext context) {
-//     return Scrollbar(
-//       controller: scrollController,
-//       child: SingleChildScrollView(
-//         controller: scrollController,
-//         child: Container(
-//           height: ,
-//         ),
+//   @factory
+//   Widget tileChild (){
+//     throw UnimplementedError();
+//   };
+//   _PageTileState createState() => _PageTileState();
+// }
+//
+// class _PageTileState extends State<PageTile> {
+//   late final List<Widget> _widget;
+//
+//   void _generateStackChildren() {
+//     _widget.add(SizedBox(height: widget.safeArea.height, width: MediaQuery.of(context).size.width, child: null));
+//     if (widget.tileBackground.isNotEmpty) {
+//       widget.tileBackground.forEach((element) {
+//         _widget.add(element);
+//       });
+//       _widget.add(Container(height: widget.safeArea.height, width: widget.safeArea.width, child: widget.child));
+//     }
+//   }
+//
+//   @override
+//   void initState() {
+//     _generateStackChildren();
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: widget.safeArea.height,
+//       width: MediaQuery.of(context).size.width,
+//       color: widget.tileBackgroundColor,
+//       child: Stack(
+//         alignment: Alignment.center,
+//         children: _widget,
 //       ),
 //     );
 //   }
-//
-//   @factory
-//   PageTile tile ()
-//
-// }
-//
-// abstract class PageTile {
-//   Widget tile (BuildContext context){
-//     return Container(
-//
-//     ),
-//   }
-//
 // }
